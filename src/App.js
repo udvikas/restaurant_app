85830dbd5fc5e725405d2b9a459dee85540dc970
Vikash Sharma <kvikashsharma09@gmail.com>
2023-04-15 20:05:39 +0530

    third cmmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { Fragment } from 'react';
-import './App.css';
-import Header from './components/Layout/Header';
+import { Fragment } from "react";
+import "./App.css";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
+
 const Header = () => {
   return (
     <Fragment>
